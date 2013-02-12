411755286d819291e63f5363ee017ac59991ed94
Takeru Inoue <takeru.inoue@gmail.com>
2013-02-12 20:28:22 +0900

    add author info etc to __init__.py

--- graphillion/__init__.py
+++ graphillion/__init__.py
@@ -1,2 +1,14 @@
 from graphillion.setset import setset
 from graphillion.graphset import GraphSet
+
+__version__ = '0.9'
+
+#__date__ = ''
+
+__author__ = '''Takeru Inoue <takeru.inoue@gmail.com>
+Shinichi Minato <minato@ist.hokudai.ac.jp>
+Hiroaki Iwashita <iwashita@erato.ist.hokudai.ac.jp>
+Takahisa Toda <toda@erato.ist.hokudai.ac.jp>
+Jun Kawahara <jkawahara@is.naist.jp>
+'''
+__license__ = 'MIT'
