119983d9d283b4ef5870dd4178bfd2afd1f74cd7
Yamazaki Hiroki <yamazaki.hiroki.58r@gmail.com>
2021-03-19 00:18:16 +0900

    refactoring

--- src/graphillion/reliability/reliability.h
+++ src/graphillion/reliability/reliability.h
@@ -2,6 +2,9 @@
  * Reliability: Computing the network reliability with TdZdd
  */
 
+#ifndef GRAPHILLION_RELIABILITY_H_
+#define GRAPHILLION_RELIABILITY_H_
+
 #include <string>
 #include <vector>
 
@@ -13,3 +16,5 @@ double reliability(const std::vector<edge_t>& edges,
                    const std::vector<double>& prob_list,
                    const std::vector<std::string>& terminals);
 }  // namespace graphillion
+
+#endif  // GRAPHILLION_RELIABILITY_H_
