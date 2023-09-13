24a7cad7ab89cd3036e95238633eaeac9f990fb5
0kina <itokon20@gmail.com>
2023-09-13 14:26:43 +0900

    Add test for VSS.cliques()

--- graphillion/test/vertex_setset.py
+++ graphillion/test/vertex_setset.py
@@ -652,6 +652,16 @@ class TestVertexSetSet(unittest.TestCase):
         self.assertTrue([2, 3, 4] in vss.minimal())
         self.assertTrue([2, 3, 5] not in vss)
 
+    def test_cliques(self):
+        GraphSet.set_universe([(1, 2), (1, 3), (1, 4), (1, 5), (1, 6)])
+        VertexSetSet.set_universe([1, 2, 3, 4, 5])
+        edges = [(1, 2), (1, 3), (1, 4), (1, 5),
+                 (2, 3), (2, 4), (2, 5), (4, 5)]
+        vss = VertexSetSet.cliques(edges)
+
+        self.assertEqual(len(vss), 20)
+        self.assertEqual(vss.maximal(), VertexSetSet([[1, 2, 3], [1, 2, 4, 5]]))
+
 #     skip tests below because networkx cannot used with VertexSetSet class now
 #     def test_networkx(self):
 #         try:
